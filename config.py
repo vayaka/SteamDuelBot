f611836f832b574c4028e8dc5aaaf99a0be4e40e
korot <korotivan2005@yandex.ru>
2023-11-28 21:31:02 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,143 @@
+from dataclasses import dataclass
+from typing import Optional
+from sqlalchemy.engine.url import URL
+
+from environs import Env
+
+
+@dataclass
+class DbConfig:
+    """
+    Database configuration class.
+    This class holds the settings for the database, such as host, password, port, etc.
+
+    Attributes
+    ----------
+    host : str
+        The host where the database server is located.
+    password : str
+        The password used to authenticate with the database.
+    user : str
+        The username used to authenticate with the database.
+    database : str
+        The name of the database.
+    port : int
+        The port where the database server is listening.
+    """
+
+    host: str
+    password: str
+    user: str
+    database: str
+    port: int = 5432
+
+    # For SQLAlchemy
+    def construct_sqlalchemy_url(self, driver="asyncpg", host=None, port=None) -> str:
+        """
+        Constructs and returns a SQLAlchemy URL for this database configuration.
+        """
+        if not host:
+            host = self.host
+        if not port:
+            port = self.port
+        uri = URL.create(
+            drivername=f"postgresql+{driver}",
+            username=self.user,
+            password=self.password,
+            host=host,
+            port=port,
+            database=self.database,
+        )
+        return uri.render_as_string(hide_password=False)
+
+    @staticmethod
+    def from_env(env: Env):
+        """
+        Creates the DbConfig object from environment variables.
+        """
+        host = env.str("DB_HOST")
+        password = env.str("DB_PASSWORD")
+        user = env.str("DB_USER")
+        database = env.str("DB_NAME")
+        port = env.int("DB_PORT", 5432)
+        return DbConfig(
+            host=host, password=password, user=user, database=database, port=port
+        )
+
+
+@dataclass
+class TgBot:
+    """
+    Creates the TgBot object from environment variables.
+    """
+
+    token: str
+    admin_ids: list[int]
+
+    @staticmethod
+    def from_env(env: Env):
+        """
+        Creates the TgBot object from environment variables.
+        """
+        token = env.str("BOT_TOKEN")
+        admin_ids = list(map(int, env.list("ADMINS")))
+        return TgBot(token=token, admin_ids=admin_ids)
+
+
+@dataclass
+class Miscellaneous:
+    """
+    Miscellaneous configuration class.
+
+    This class holds settings for various other parameters.
+    It merely serves as a placeholder for settings that are not part of other categories.
+
+    Attributes
+    ----------
+    other_params : str, optional
+        A string used to hold other various parameters as required (default is None).
+    """
+
+    other_params: str = None
+
+
+@dataclass
+class Config:
+    """
+    The main configuration class that integrates all the other configuration classes.
+
+    This class holds the other configuration classes, providing a centralized point of access for all settings.
+
+    Attributes
+    ----------
+    tg_bot : TgBot
+        Holds the settings related to the Telegram Bot.
+    misc : Miscellaneous
+        Holds the values for miscellaneous settings.
+    db : Optional[DbConfig]
+        Holds the settings specific to the database (default is None).
+    """
+
+    tg_bot: TgBot
+    misc: Miscellaneous
+    db: Optional[DbConfig] = None
+
+
+def load_config(path: str = None) -> Config:
+    """
+    This function takes an optional file path as input and returns a Config object.
+    :param path: The path of env file from where to load the configuration variables.
+    It reads environment variables from a .env file if provided, else from the process environment.
+    :return: Config object with attributes set as per environment variables.
+    """
+
+    # Create an Env object.
+    # The Env object will be used to read environment variables.
+    env = Env()
+    env.read_env(path)
+
+    return Config(
+        tg_bot=TgBot.from_env(env),
+        db=DbConfig.from_env(env),
+        misc=Miscellaneous(),
+    )
---
+++ main.py
@@ -0,0 +1,84 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from config import load_config, Config
+from bot.handlers import routers_list
+from bot.middlewares.config import ConfigMiddleware
+from bot.services import broadcaster
+
+
+async def on_startup(bot: Bot, admin_ids: list[int]):
+    await broadcaster.broadcast(bot, admin_ids, "The bot was launched!")
+
+
+def register_global_middlewares(dp: Dispatcher, config: Config, session_pool=None):
+    """
+    Register global middlewares for the given dispatcher.
+    Global middlewares here are the ones that are applied to all the handlers (you specify the type of update)
+
+    :param dp: The dispatcher instance.
+    :type dp: Dispatcher
+    :param config: The configuration object from the loaded configuration.
+    :param session_pool: Optional session pool object for the database using SQLAlchemy.
+    :return: None
+    """
+    middleware_types = [
+        ConfigMiddleware(config),
+        DatabaseMiddleware(session_pool),
+    ]
+
+    for middleware_type in middleware_types:
+        dp.message.outer_middleware(middleware_type)
+        dp.callback_query.outer_middleware(middleware_type)
+
+
+def setup_logging():
+    """
+    Set up logging configuration for the application.
+
+    This method initializes the logging configuration for the application.
+    It sets the log level to INFO and configures a basic colorized log for
+    output. The log format includes the filename, line number, log level,
+    timestamp, logger name, and log message.
+
+    Returns:
+        None
+
+    Example usage:
+        setup_logging()
+    """
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s",
+    )
+    logger = logging.getLogger(__name__)
+    logger.info("Starting bot")
+
+
+async def main():
+    setup_logging()
+
+    config = load_config(".env")
+    async_engine = create_engine(config.db, echo=False)
+    session_pool = create_session_pool(async_engine)
+
+    bot = Bot(token=config.tg_bot.token, parse_mode="HTML")
+    dp = Dispatcher(storage=MemoryStorage())
+
+    dp.include_routers(*routers_list)
+
+    register_global_middlewares(dp, config)
+
+    await on_startup(bot, config.tg_bot.admin_ids)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logging.error("The bot has been stopped!")
